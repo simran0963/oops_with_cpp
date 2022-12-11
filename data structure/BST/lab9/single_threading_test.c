e4247fc5122a1284f1a84877d5498cf92c83cbb5
simran0963 <simranmehta0963@gmail.com>
2022-12-11 16:48:55 +0530

    commit bst

--- data structure/BST/lab9/single_threading_test.c
+++ data structure/BST/lab9/single_threading_test.c
@@ -4,6 +4,40 @@
 
 struct node{
 	int data;
-	struct node *right, *left;
+	struct node *rc, *lc;
 	bool tread;
 };
+
+int main(){                                           // main function
+ 	struct node *head = NULL;
+  	int ch = 0,n;
+ 	
+	while(1){
+		printf("\n1 - Insert\n2 - Display\n3 - Delete\n4 - Exit\nEnter your Choice : ");
+		scanf("%d",&ch);
+		switch (ch){
+			case 1:
+				printf("Element : ");
+				scanf("%d",&n);
+				head = insert(head,n);
+				break;
+				
+			case 2:
+				traverse(head);
+				break;
+				
+			case 3:
+				printf("Element : ");
+				scanf("%d",&n);
+				head = deleteNode(head, n);
+				break; 
+				
+			case 4:
+				exit(0);	
+				
+			default:
+				printf("\nInvalid Choice !\n");
+		}
+	}
+	return 0;
+}
